3e0444530fff4863618145aefe2d351f3ffdf153
yfontanet <yaiza.fontanet222@comunidadunir.net>
2026-05-29 12:10:53 +0200

    corrección NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import FuelTable from './components/FuelTable';
 import Register from './components/Register';
 import Login from './components/Login';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 
 // Componente principal de la aplicación
 // Este componente es el punto de entrada de la aplicación y se encarga de gestionar las rutas y el estado global de la aplicación.
@@ -62,23 +63,20 @@ function App() {
   return (
     <BrowserRouter>
       <Header user={user} />
-      {
-        loading && <div className="loading">Cargando...</div>
-      }
-      {
-        error && <div className="error">Error: {error}</div>
-      }
-      {!loading && !error && (
-        <Routes>
-          <Route path="/registro" element={<Register />} />
-          <Route path="/login" element={<Login onLogin={setUser} />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Home stations={stations} />} />
-          <Route path="/mapa" element={<FuelMap stations={stations} />} />
-          <Route path="/lista" element={<FuelTable stations={stations} />} />
-          <Route path="/station/:id" element={<StationDetail stations={stations} user={user} />} />
-        </Routes>
-      )}
+      {loading && <div className="loading">Cargando...</div>}
+      {error && <div className="error">Error: {error}</div>}
+      <Routes>
+        <Route path="/registro" element={<Register />} />
+        <Route path="/login" element={<Login onLogin={setUser} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home stations={stations} />} />
+        <Route path="/mapa" element={<FuelMap stations={stations} />} />
+        <Route path="/lista" element={<FuelTable stations={stations} />} />
+        <Route path="/station/:id" element={<StationDetail stations={stations} user={user} />} />
+        <Route path="/ruta-inexistente" element={<NotFound />} />
+        <Route path="/xxx" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   )
